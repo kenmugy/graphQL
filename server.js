7d012bb3f396b24a9aa92d5b5832e56ee442059e
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-29 15:41:54 +0300

    added custom objects

--- server.js
+++ server.js
@@ -5,19 +5,52 @@ const { buildSchema } = require('graphql');
 const app = express();
 const port = 4000;
 
+const fakeData = {
+  '1': {
+    id: '1',
+    name: 'alex jones',
+    age: Math.trunc(Math.random() * 50) + 1,
+    random: Math.random * 3,
+    friends: ['friend 1', 'friend 2', 'friend 3', 'friend 4'],
+  },
+  '2': {
+    id: '2',
+    name: 'black adam',
+    age: Math.trunc(Math.random() * 50) + 1,
+    random: Math.random * 3,
+    friends: ['friend 1', 'friend 2', 'friend 3', 'friend 4'],
+  },
+  '3': {
+    id: '3',
+    name: 'cat woman',
+    age: Math.trunc(Math.random() * 50) + 1,
+    random: Math.random * 3,
+    friends: ['friend 1', 'friend 2', 'friend 3', 'friend 4'],
+  },
+  '4': {
+    id: '4',
+    name: 'dare devil',
+    age: Math.trunc(Math.random() * 50) + 1,
+    random: Math.floor(Math.random * 3) + 1,
+    friends: ['friend 1', 'friend 2', 'friend 3', 'friend 4'],
+  },
+};
+
 const schema = buildSchema(`
+    type User {
+      id:String,
+      name:String,
+      age: Int,
+      random:Float,
+      friends: [String]
+    }
+
     type Query {
-        name: String,
-        age: Int,
-        random: Float,
-        friends: [String]
+        user(id:String): User
     }
 `);
 const root = {
-  name: () => 'Ken Mugy',
-  age: () => 31,
-  random: () => Math.floor(Math.random() * 6) + 1,
-  friends: () => ['adrian', 'ariana', 'grande'],
+  user: ({ id }) => fakeData[id],
 };
 
 app.use(
